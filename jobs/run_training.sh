9ed479a2242a61684835391a7c73dbdf932c1599
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-01-26 13:43:13 +0000

    job scripts (using node 1)

---
+++ jobs/run_training.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p GPU
+#SBATCH -N1
+#SBATCH -c4
+#SBATCH --gres=gpu:v100:1
+#SBATCH --nodelist=compute-gpu-0-1
+#SBATCH --mail-user=alexander.wilkinson.20@ucl.ac.uk
+#SBATCH --mail-type=END,FAIL
+
+echo run_training.sh
+cd /home/awilkins/extrapolation_pix2pix
+module load gcc/8.3.0
+source /share/apps/anaconda/3-2019.03/etc/profile.d/conda.sh
+conda activate 
+conda activate pytorch-CycleGAN-and-pix2pix
+python train.py
+
