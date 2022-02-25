5393eeb61f528388689a467fad0024259fae1eef
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-02-25 15:34:14 +0000

    job script to tarball datset

---
+++ jobs/tar_dset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p GPU
+#SBATCH -N1
+#SBATCH -c2
+#SBATCH --nodelist=compute-gpu-0-1
+#SBATCH --mail-user=alexander.wilkinson.20@ucl.ac.uk
+#SBATCH --mail-type=END,FAIL
+
+echo tar_dset.sh
+cd /state/partition1/awilkins
+tar -czf nd_fd_radi_1-8_vtxaligned_noped.tar.gz nd_fd_radi_1-8_vtxaligned/
+
+
