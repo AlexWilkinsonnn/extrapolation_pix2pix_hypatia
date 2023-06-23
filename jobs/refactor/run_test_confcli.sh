509fb8ca3ea6d2256c10edc95e021fbab0b2be2a
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2023-06-23 17:52:40 +0100

    slurm script for submitting model testing jobs

---
+++ jobs/refactor/run_test_confcli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p GPU
+#SBATCH -N1
+#SBATCH -c4
+#SBATCH --gres=gpu:1
+#SBATCH --error=/home/awilkins/extrapolation_pix2pix/jobs/logs/err/job%j.err
+#SBATCH --output=/home/awilkins/extrapolation_pix2pix/jobs/logs/out/job%j.out
+
+cd /home/awilkins/extrapolation_pix2pix
+module load gcc/8.3.0
+source /share/apps/anaconda/3-2019.03/etc/profile.d/conda.sh
+conda activate 
+conda activate pytorch-CycleGAN-and-pix2pix
+python test_new.py --epoch $2 $1
