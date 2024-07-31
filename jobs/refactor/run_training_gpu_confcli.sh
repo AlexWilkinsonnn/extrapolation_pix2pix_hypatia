9d3bfce5ca74476553e221aff68ee919468b6f78
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-07-31 17:18:17 +0100

    update slurm submit script

--- jobs/refactor/run_training_gpu_confcli.sh
+++ jobs/refactor/run_training_gpu_confcli.sh
@@ -2,13 +2,19 @@
 #SBATCH -p GPU
 #SBATCH -N1
 #SBATCH -c4
-#SBATCH --gres=gpu:1
-#SBATCH --error=/home/awilkins/extrapolation_pix2pix/jobs/logs/err/job%j.err
-#SBATCH --output=/home/awilkins/extrapolation_pix2pix/jobs/logs/out/job%j.out
+#SBATCH --gres=gpu:a100:1
+#SBATCH --nodelist=compute-gpu-0-2
+#SBATCH --error=/home/awilkins/extrapolation_pix2pix/jobs/logs/err/job.%x.%j.err
+#SBATCH --output=/home/awilkins/extrapolation_pix2pix/jobs/logs/out/job.%x.%j.out
+
+CONFIG_FILE=$1
+
+echo $SLURMD_NODENAME
+echo $CUDA_VISIBLE_DEVICES
+echo $CONFIG_FILE
 
 cd /home/awilkins/extrapolation_pix2pix
 module load gcc/8.3.0
-source /share/apps/anaconda/3-2019.03/etc/profile.d/conda.sh
-conda activate 
-conda activate pytorch-CycleGAN-and-pix2pix
-python train.py $1
+source setups/setup.sh
+
+python train.py $CONFIG_FILE
